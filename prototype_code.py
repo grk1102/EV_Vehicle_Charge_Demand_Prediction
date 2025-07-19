9ab95abd070cdb13d408c9c6e55c61ff14273312
MounikaJeede <jeedemounika@gmail.com>
2025-07-19 11:56:01 +0530

    Initial commit: Added EV population dataset and code

---
+++ prototype_code.py
@@ -0,0 +1,38 @@
+import joblib
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+# Load data
+df = pd.read_csv("Electric_Vehicle_Population_By_County.csv")
+
+df.head() # top 5 rows
+
+# no of rows and cols
+df.shape
+
+# Data Types, class and memory alloc
+df.info()
+
+df.isnull().sum()
+
+# Compute Q1 and Q3
+Q1 = df['Percent Electric Vehicles'].quantile(0.25)
+Q3 = df['Percent Electric Vehicles'].quantile(0.75)
+IQR = Q3 - Q1
+
+# Define outlier boundaries
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+print('lower_bound:', lower_bound)
+print('upper_bound:', upper_bound)
+
+# Identify outliers
+outliers = df[(df['Percent Electric Vehicles'] < lower_bound) | (df['Percent Electric Vehicles'] > upper_bound)]
+print("Number of outliers in 'Percent Electric Vehicles':", outliers.shape[0])
